4bb6813cb8ab4eeb6e69b97157cbfc0a08239334
Alastair <alsgregory@aol.com>
2016-07-29 09:00:42 +0100

    changes to travis. also changed state

--- firedrake_mlmc/state.py
+++ firedrake_mlmc/state.py
@@ -11,12 +11,10 @@ class State(object):
 
     """ Tuple that stores the state :class:`Function` and properties of them for the coarse (index 0) and fine (index 1) discretizations for a single realisation of the user define system
 
-        :param input_1: Coarse state :class:`Function` needed for discretization.
-                        Can be a :attr:`list` of multiple :class:`Function`
+        :param input_1: Coarse state :class:`Function` needed for discretization
         :type input_1: :class:`Function`
 
-        :param input_2: Fine state :class:`Function` needed for discretization.
-                        Can be a :attr:`list` of multiple :class:`Function`
+        :param input_2: Fine state :class:`Function` needed for discretization
         :type input_2: :class:`Function`
 
     """
@@ -28,18 +26,23 @@ class State(object):
 
         # give the state the attributes the levels of each fine / coarse
         # solution. be careful for states which are lists of multiple functions.
-        if isinstance(self.state, list):
-            # the levels corresponding to the coarse and fine state :class:`Function`.
-            self.levels = tuple([get_level(self.state[0][0])[1],
-                                 get_level(self.state[1][0])[1]])
-
-        else:
-            self.levels = tuple([get_level(self.state[0])[1],
+        self.levels = tuple([get_level(self.state[0])[1],
                                 get_level(self.state[1])[1]])
+
+        # add check that both inputs are Functions
+        if type(self.state[0]) != Function:
+            raise TypeError('state input is not a Function')
+
+        if type(self.state[1]) != Function:
+            raise TypeError('state input is not a Function')
 
         # add check for levels - non fatal -> perhaps change to actual error
         if self.levels[0] == -1 or self.levels[1] == -1:
             raise Warning('Levels of state may not be actual hierarchal levels.' +
                           ' Check if they belong to FunctionHierarchy! get_level has failed.')
+
+        # check for non consecutive levels
+        if self.levels[1] - 1 != self.levels[0]:
+            raise ValueError('levels of inputs are not consecutive')
 
         super(State, self).__init__()
