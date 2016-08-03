34e960a80ffe0cb98784c0a165a9745e67bacaa6
Alastair <alsgregory@aol.com>
2016-08-03 13:08:31 +0100

    corrected init

--- firedrake_mlmc/__init__.py
+++ firedrake_mlmc/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
 from firedrake_mlmc.state import *  # noqa
-from firedrake_mlmc.ensemble_hierarchy import *  # noqa
 from firedrake_mlmc.min_dx import *  # noqa
 from firedrake_mlmc.generalised_hierarchy import *  # noqa
